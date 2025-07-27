fd96478f564c040952077008e7974557467401dc
Rampeddireddi <rampeddireddi1@gmail.com>
2025-07-27 14:54:49 +0530

    allowing server to listen all

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.get("/",(req,res)=>{res.send("hello")})
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0',() => console.log(`Server running on port ${PORT}`));
